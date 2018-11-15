3e94fd1fc29bf3f420c133589da4b7dcaf1c1503
Robert Dragan <drakenshot23@github.com>
2018-11-15 22:58:47 +0100

    initial commit with redux setup

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {}
+
+const middleware = [ReduxThunk];
+
+const store = createStore(rootReducer, initialState, applyMiddleware(...middleware));
+
+export default store;
